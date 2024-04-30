1fe7f53e3394817196d8e16a72b8a0615146d1f8
Commissioner Gordon <geden11@gmail.com>
2024-04-29 20:55:14 -0700

    problem solved

--- db/storage.js
+++ db/storage.js
@@ -1,50 +1,58 @@
-const util = require('util');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-const { parse } = require('path');
+const util = require("util");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
 
 const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.watchFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
-    read(){
-        return readFileAsync('./db/db.json','utf8')
-    }
-    markDown(notes){
-        return writeFileAsync('./db/db.json',JSON.stringify(notes, null, 4))
-    }
-    retrieveNotes(){
-        return this.read().then((notes) => {
-            let parsedNotes;
-            try {
-                parsedNotes = [].concat(JSON.parse(notes))
-            }
-            catch (error) {
-                parsedNotes =[]
-            }
-            return parsedNotes;
-        })
-    }
-    appendNote(notes){
-        const {title, text} = notes
-        
+  read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
+  markDown(notes) {
+    console.log(notes)
+    return writeFileAsync("db/db.json", JSON.stringify(notes, null, 4));
+  }
+  retrieveNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (error) {
+        parsedNotes = [];
+      }
+      return parsedNotes;
+    });
+  }
+  appendNote(notes) {
+    const { title, text } = notes;
 
-        if (!title || !text) {
-            throw new Error ('needs title and notes')
-        }
-        const nextNote = { title, text, id: uuidv4()}
-        return this.retrieveNotes().then((notes) => [
-            ...notes, nextNote
-        ]).then((updateNotes) => this.markDown(updateNotes))
-        .then(()=> nextNote);
+    if (!title || !text) {
+      throw new Error("needs title and notes");
     }
-    deleteNote(id){
-        return this.retrieveNotes().then((notes) => notes.filter((note) => note.id !== id))
-        .then((filteredNotes) => this.markDown(filteredNotes))
+    const nextNote = { title, text, id: uuidv4() };
+    //console.log(nextNote);
+    return this.retrieveNotes()
+      .then((currentNotes) => {
+        const updateNotes =[...currentNotes, nextNote]
+    //console.log('updatedNotes', updateNotes)
+return updateNotes})
+      .then((updateNotes) => {
+          
+        //  console.log('new markdown created')
+         return this.markDown(updateNotes)
 
-    }
-};
+})
+      .then(() => nextNote);
+  }
+  deleteNote(id) {
+    return this.retrieveNotes()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((filteredNotes) => this.markDown(filteredNotes));
+  }
+}
 
-const a = new Store().retrieveNotes().then((notes) => console.log(notes));
+//const a = new Store().retrieveNotes().then((notes) => console.log(notes));
 //const a = new Store().retrieveNotes().then((data) => data.JSON()).then((notes) => console.log(notes))
 module.exports = new Store();
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
+const storage = require('./db/storage')
+
 const app = express();
-const PORT = process.env.PORT || 3202;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'))
 
-const storage = require('./db/storage')
+
 app.get(`/api/notes`,(req,res) =>{
     storage.retrieveNotes().then((notes) => {
         return res.status(200).json(notes)
@@ -13,6 +17,7 @@ app.get(`/api/notes`,(req,res) =>{
 });
 app.post('/api/notes',(req,res) => {
     storage.appendNote(req.body).then((notes) => {
+        console.log(notes)
         return res.status(200).json(notes)
     }).catch((error) => res.status(500).json(error));
 })
@@ -22,6 +27,14 @@ app.delete('/api/notes/:id',(req, res) => {
         return res.status(200).json({delete: true, id: req.params.id})})
         .catch((error) => res.status(500).json(error))
 })
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 app.listen(PORT, ()=> {
     console.log(`Server is running on http://localhost:${PORT}`);
 } );
